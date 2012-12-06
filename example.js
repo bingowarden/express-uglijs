1fa35de662733061ad9a6e95587357e5c99d1848
bingowarden <34237598@qq.com>
2012-12-06 16:14:35 +0800

    edit example.js

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ var app = express();
 var UglifyJS = require("express-uglijs");
 app.use('/static/js',UglifyJS.oldUGJS(__dirname+'/resources/js','cache'));
 //UglifyJS.oldUGJS(path,cachePath);
+//UglifyJS.newUGJS(path,cachePath);
 //path为文件实际路径，cachePath为首次压缩后生成的压缩文件保存路径，默认在path目录下
 //前台HTML代码<script src="static/js/jquer.js|index.js|others.js"></script>
 
